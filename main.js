572c17c670c4e3984271cc69921f663b0c325f72
Yhago Salim <yhagosalim.bastos@estudante.ufjf.br>
2023-12-06 16:20:30 -0300

    adiciona jogaDados

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-import { getJogador } from "./tabuleiro.js";
+import { getValorJogador, getValorComputador } from "./tabuleiro.js";
 
-function jogaDados(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function jogaDados() {
+    let min, max;
+    let resultado;
+    min = Math.ceil(1);
+    max = Math.floor(6);
+    resultado = Math.floor(Math.random() * (max - min + 1)) + min;
+    return resultado;
+    // mostraResultado;
+    // escreveResultadoNaTela(resultado);
   }
 
   console.log("Dado: ", jogaDados(1, 6));
+  console.log("jogador: ", getValorJogador(1, 1));
+  console.log("computador: ", getValorComputador(1, 1));
