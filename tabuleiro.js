ca84a5306763027582f4702ba4426aba20ea09ea
Yhago Salim <yhagosalim.bastos@estudante.ufjf.br>
2023-12-06 16:20:12 -0300

    conserta estado

--- tabuleiro.js
+++ tabuleiro.js
@@ -1,7 +1,17 @@
 let jogador = [ [" ", " ", " "],
-                [" ", " " , " "],
+                [" ", 5, " "],
                 [" ", " ", " "] ];
 
 let computador = [ [" ", " ", " "],
-                    [" ", " ", " "],
+                    [" ", 6, " "],
                     [" ", " ", " "] ];
+
+function getValorJogador(lin, col) {
+  return jogador[lin][col];
+}
+
+function getValorComputador(lin, col) {
+  return computador[lin][col];
+}
+
+export { getValorJogador, getValorComputador };
