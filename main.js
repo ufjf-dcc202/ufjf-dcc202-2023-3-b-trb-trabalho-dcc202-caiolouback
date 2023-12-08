661885e36b8d536529530f3956e3d86c0c64c7e3
Yhago Salim <yhagosalim.bastos@estudante.ufjf.br>
2023-12-08 15:16:04 -0300

    adiciona funções de comportamento

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
-import { getValorJogador, getValorComputador } from "./tabuleiro.js";
+import { getJogador, getComputador, getPontosJogador, getPontosComputador, jogaDado, calculaPontosColuna, calculaPontos, verificaFimDeJogo } from "./tabuleiro.js";
 
-const btnJogarDado = document.querySelector("#jogarDado");
+const btnIniciaJogo = document.querySelector("#iniciaJogo");
 
-btnJogarDado.addEventListener('click', jogaDados);
+let jog = getJogador(); //matriz jogador
+let comp = getComputador(); //matriz computador
+let jogPontos = getPontosJogador();
+let compPontos = getPontosComputador();
 
-function jogaDados() {
-    let min, max;
-    let resultado;
-    min = Math.ceil(1);
-    max = Math.floor(6);
-    resultado = Math.floor(Math.random() * (max - min + 1)) + min;
-    console.log("Dado: ", resultado);
-    // mostraResultado;
-    // escreveResultadoNaTela(resultado);
+btnIniciaJogo.addEventListener('click', iniciarJogo);
+
+console.log(verificaFimDeJogo());
+
+function iniciarJogo() {
+  for(let i=1; i<200; i++) {
+    if(i%2 !== 0) {
+      //vez do computador
+    }
+    if(verificaFimDeJogo()) {
+      break;
+    }
+    //atualizaTela();
+    i++;
+    if(i%2 === 0) {
+      //vez do Jogador
+    }
+    //atualizaTela();
+    if(verificaFimDeJogo()) {
+      break;
+    }
   }
-
-  console.log("jogador: ", getValorJogador(1, 1));
-  console.log("computador: ", getValorComputador(1, 1));
+}
--- tabuleiro.js
+++ tabuleiro.js
@@ -6,12 +6,76 @@ let computador = [ [" ", " ", " "],
                     [" ", " ", " "],
                     [" ", " ", " "] ];
 
-function getValorJogador(lin, col) {
-  return jogador[lin][col];
+let pontosJogador = 0;
+let pontosComputador = 0;
+
+function getJogador() {
+  return jogador;
 }
 
-function getValorComputador(lin, col) {
-  return computador[lin][col];
+function getComputador() {
+  return computador;
 }
 
-export { getValorJogador, getValorComputador };
+function getPontosJogador() {
+  return pontosJogador;
+}
+
+function getPontosComputador() {
+  return pontosComputador;
+}
+
+function jogaDado() {
+  let min, max;
+  let resultado;
+  min = Math.ceil(1);
+  max = Math.floor(6);
+  resultado = Math.floor(Math.random() * (max - min + 1)) + min;
+  return resultado;
+}
+
+function calculaPontosColuna(mat, col) {
+  let somaPontosColuna = 0, multiplicador = 1;
+  for(let i=0; i<3; i++) {
+    somaPontosColuna += mat[i][col];
+  }
+  if(mat[0][col] === mat[1][col] && mat[0][col] === mat[2][col]) {
+    multiplicador = 3;
+  }
+  else if(mat[0][col] === mat[1][col] || mat[0][col] === mat[2][col] || mat[1][col] === mat[2][col]) {
+      multiplicador = 2;
+    }
+  somaPontosColuna = somaPontosColuna * multiplicador;
+  return somaPontosColuna;
+}
+
+function calculaPontos(matriz) {
+  let somaPontos = 0;
+  somaPontos = calculaPontosColuna(matriz, 0) + calculaPontosColuna(matriz, 1) + calculaPontosColuna(matriz, 2);
+  return somaPontos;
+}
+
+function verificaFimDeJogo() {
+  let tabuleiroCompleto = false;
+  let casasVaziasJogador = 0, casasVaziasComputador = 0;
+  for(let i=0; i<3; i++) {
+    for(let j=0; j<3; j++) {
+      if(jogador[i][j] === " ") {
+        casasVaziasJogador++;
+      }
+    }
+  }
+  for(let i=0; i<3; i++) {
+    for(let j=0; j<3; j++) {
+      if(computador[i][j] === " ") {
+        casasVaziasComputador++;
+      }
+    }
+  }
+  if( (casasVaziasJogador === 0) || (casasVaziasComputador === 0) ) {
+    tabuleiroCompleto = true;
+  }
+  return tabuleiroCompleto;
+}
+
+export { getJogador, getComputador, getPontosJogador, getPontosComputador, jogaDado, calculaPontosColuna, calculaPontos, verificaFimDeJogo };
