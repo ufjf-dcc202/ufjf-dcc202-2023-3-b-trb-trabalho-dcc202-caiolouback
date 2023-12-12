578d4951ce568f5f0409a744ed990d508f80c3ec
Yhago Salim <yhagosalim.bastos@estudante.ufjf.br>
2023-12-12 16:41:38 -0300

    aprimora o comportamento do jogo e define comportamento para o computador

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 import { getJogador, getComputador, getPontosJogador, getPontosComputador, aleatorizaNum, 
   jogaDado, atualizaPontos, escreveNaTabelaJogador, escreveNaTabelaComputador, verificaFimDeJogo, 
-  verificaCounterJogador, verificaCounterComputador } from "./tabuleiro.js";
+  verificaCounterJogador, verificaCounterComputador, contaCasasVazias, 
+  verificaVencedor } from "./tabuleiro.js";
 
 const btnIniciaJogo = document.querySelector("#iniciaJogo");
 
+let jog = getJogador(); 
+let comp = getComputador();
 let jogPontos = getPontosJogador();
 let compPontos = getPontosComputador();
 
@@ -16,8 +19,8 @@ button.onclick = function() {  //função para botão desaparecer depois de clic
 }
 
 function atualizaTela() {
-  let jog = getJogador(); //matriz jogador
-  let comp = getComputador(); //matriz computador
+  jog = getJogador(); 
+  comp = getComputador();
   //escreve matrizes na tela
   atualizaPontos();
   jogPontos = getPontosJogador();
@@ -26,29 +29,62 @@ function atualizaTela() {
 }
 
 function iniciarJogo() {
+  let fimDeJogo = false;
   atualizaTela();
-  for(let i=1; i<200; i++) {
+  while(fimDeJogo === false) {
+    let i=1;
     if(i%2 !== 0) {
       //vez do computador
-      let res1, col1;
-      res1 = jogaDado();
-      col1 = aleatorizaNum(0, 2);
-      // escreveNaTabelaComputador(res1, col1);
+      let resComp, colComp;
+      let iComp, jComp, kComp;
+      comp = getComputador();
+      resComp = jogaDado();
+      iComp = contaCasasVazias(comp, 0);
+      jComp = contaCasasVazias(comp, 1);
+      kComp = contaCasasVazias(comp, 2);
+      if(iComp > 0) {
+        colComp = 0;
+      } else if(jComp > 0) {
+        colComp = 1;
+      } else if(kComp > 0) {
+        colComp = 2;
+      }
+      escreveNaTabelaComputador(resComp, colComp);
+      verificaCounterComputador(resComp, colComp);
     }
     atualizaTela();
     if(verificaFimDeJogo()) {
+      fimDeJogo = true;
       break;
     }
     i++;
     if(i%2 === 0) {
       //vez do Jogador
-      let res2, col2;
-      res2 = jogaDado();
+      let resJog, colJog;
+      let iJog, jJog, kJog;
+      jog = getJogador();
+      iJog = contaCasasVazias(jog, 0);
+      jJog = contaCasasVazias(jog, 1);
+      kJog = contaCasasVazias(jog, 2);
+      resJog = jogaDado();
+      // escreveNaTabelaJogador(resJog, colJog);
+      // verificaCounterJogador(resJog, colJog);
     }
     atualizaTela();
     if(verificaFimDeJogo()) {
+      fimDeJogo = true;
       break;
     }
+    i++;
   }
-  // verificaVencedor();
+  let vencedor = verificaVencedor();
+  if(vencedor === 0) {
+    // empate
+  }
+  else if(vencedor === 1) {
+    // computador
+  }
+  else if(vencedor === 2) {
+    // jogador
+  }
 }
